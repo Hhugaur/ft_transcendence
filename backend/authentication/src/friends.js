0184d5bcee6c9207d691b302ac331500de2a84d3
Romain-ItAllDepends <romain.gobet30@hotmail.com>
2025-07-22 15:58:43 +0200

    Add friend

---
+++ backend/authentication/src/friends.js
@@ -0,0 +1,17 @@
+import { config } from './config.js';
+
+export const sendDbAddFriendRequest = async (username, friend) => {
+    const send = await fetch(config.databaseUrl + '/friends/add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, friend }),
+    });
+
+    if (!send.ok) {
+        const error = await send.text();
+        console.log('Sending to DB:', JSON.stringify({ username, friend }));
+        throw new Error(`Error API DB: ${error}`);
+    }
+}
