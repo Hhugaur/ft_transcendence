271d50d0103cc998621f544e216b915ea9c4a7c6
Romain-ItAllDepends <romain.gobet30@hotmail.com>
2025-09-29 10:47:41 +0200

    disconnect but bugged

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,22 +1,27 @@
 export async function sendRequest(url: string, nameVar1: string, nameVar2: string, var1: string, var2: string) {
     try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ [nameVar1]: var1, [nameVar2]: var2 })
-        });
-
-        const data = await response.json().catch(() => null);
-        if (!response.ok) {
-            const message = data?.error || `Erreur HTTP : ${response.status}`;
-            throw new Error(message);
+        if (nameVar2 !== null) {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ [nameVar1]: var1, [nameVar2]: var2 })
+            });
+            await responseTraitment(response, url);
+        } else {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ [nameVar1]: var1 })
+            });
+            await responseTraitment(response, url);
         }
 
-        console.log(selectMessage(url, true, null), data);
-        alert(selectMessage(url, true, null));
-        return data;
+
+
 
     } catch (error) {
         console.error(selectMessage(url, false, error));
@@ -25,6 +30,17 @@ export async function sendRequest(url: string, nameVar1: string, nameVar2: strin
     }
 }
 
+export async function responseTraitment(response: Response, url: string) {
+    const data = await response.json().catch(() => null);
+    if (!response.ok) {
+        const message = data?.error || `Erreur HTTP : ${response.status}`;
+        throw new Error(message);
+    }
+
+    console.log(selectMessage(url, true, null), data);
+    alert(selectMessage(url, true, null));
+    return data;
+}
 
 function selectMessage(url: string, type: boolean, error: string) {
     if (url === 'https://transcendence.42.fr:42069/api/friends/add' && type === true)
@@ -39,5 +55,9 @@ function selectMessage(url: string, type: boolean, error: string) {
         return 'Inscription réussie!';
     if (url === 'https://transcendence.42.fr:42069/api/auth/register' && type === false)
         return 'Erreur lors de l’inscription :' + error;
+    if (url === 'https://transcendence.42.fr:42069/api/auth/disconnect' && type === true)
+        return "Déconnexion réussie!";
+    if (url === 'https://transcendence.42.fr:42069/api/auth/disconnect' && type === false)
+        return "Erreur lors de la déconnexion :" + error;
 }
 // https://transcendence.42.fr:42069/api/upload
