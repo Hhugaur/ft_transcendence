078299f13eb3167f7193f79c80e341e73dd50fe4
Romain-ItAllDepends <romain.gobet30@hotmail.com>
2025-10-07 14:54:03 +0200

    cllean useless things

--- backend/authentication/src/server.js
+++ backend/authentication/src/server.js
@@ -10,7 +10,7 @@ const server = Fastify({
 await server.register(cookie);
 
 await server.register(jwt, {
-    secret: config.jwtPass, // ex: process.env.JWT_SECRET
+    secret: config.jwtPass,
     sign: {
         expiresIn: config.jwtExp || '1h'
     }
@@ -18,12 +18,7 @@ await server.register(jwt, {
 
 server.decorate("authenticate", async function (request, reply) {
     try {
-        // vérifie le cookie d'abord (si tu settes dans cookie)
-        if (request.cookies && request.cookies.token) {
-            await request.jwtVerify({ cookie: { cookieName: 'token' } });
-            return;
-        }
-        await request.jwtVerify(); // va throw si pas de token valide dans Authorization header
+        await request.jwtVerify();
     } catch (err) {
         reply.code(401).send({ error: 'Unauthorized' });
     }
